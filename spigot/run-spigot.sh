9a8464939aa6893c63a762ef728b190af61d8435
Sean Johnson <sean@ttys0.net>
2019-08-25 17:47:11 -0500

    minor shell script syntax update
    Signed-off-by: Sean Johnson <sean@ttys0.net>

--- spigot/run-spigot.sh
+++ spigot/run-spigot.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-for WORLD in world world_nether world_the_end
-do
+for WORLD in world world_nether world_the_end; do
   if [ ! -f ${SPIGOT_DIR}/${WORLD}/level.dat ]; then
     echo "Empty ${WORLD}! Restoring!"
     cp -r ${SPIGOT_DIR}/backups/${WORLD}/* ${SPIGOT_DIR}/${WORLD}/
@@ -9,4 +8,3 @@ do
 done
 
 exec java $JVM_OPTS -Dcom.mojang.eula.agree=true -jar /minecraft/spigot.jar "${SPIGOT_OPTS}"
-
