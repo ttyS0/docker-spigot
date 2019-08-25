bd1ef2db03d901bbdeae28753ce9870a646e9026
Sean Johnson <sean@ttys0.net>
2019-08-25 17:46:02 -0500

    Minecraft server is back up and running, with DynMap this time.
    Bungeecord defaults to the "old" world until I can get a Lobby built.
    
    Signed-off-by: Sean Johnson <sean@ttys0.net>

---
+++ spigot/run-spigot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for WORLD in world world_nether world_the_end
+do
+  if [ ! -f ${SPIGOT_DIR}/${WORLD}/level.dat ]; then
+    echo "Empty ${WORLD}! Restoring!"
+    cp -r ${SPIGOT_DIR}/backups/${WORLD}/* ${SPIGOT_DIR}/${WORLD}/
+  fi
+done
+
+exec java $JVM_OPTS -Dcom.mojang.eula.agree=true -jar /minecraft/spigot.jar "${SPIGOT_OPTS}"
+
